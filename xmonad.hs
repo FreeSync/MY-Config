0e4f895b0cafa1bc550681b0aaeee5d03bab8497
Arman Hossajn <50828365+FreeSync@users.noreply.github.com>
2020-09-21 10:36:58 +0600

    Add files via upload

--- xmonad.hs
+++ xmonad.hs
@@ -73,7 +73,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch dmenu
-    , ((modm,               xK_d     ), spawn "rofi -show drun")
+    , ((modm,               xK_d     ), spawn "rofi -show drun -show-icons")
 
 
     -- launch dmenu
